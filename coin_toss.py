472d71aedb22affb111adb45a8e6517eb2357903
Anpegal-77 <agalindez2@fingerlakes.edu>
2025-10-12 10:27:02 -0400

    Update coin_toss.py

--- coin_toss.py
+++ coin_toss.py
@@ -1,21 +1,26 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
+# NAME: Andy Galindez
+# DATE:  10/12/2025
 # BRIEF DESCRIPTION:  
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
 ########## ENTER YER CODE BELOW THIS LINE ##########
+import random
 
+def main():
+    coin_toss()
 
+def coin_toss():
+    print('===== Coin Flipper =====')
+    number=random.randint(1,100)
+    if number >=51:
+        print("Tails")
+    else:
+        print("Heads")
 
-
-
-
-
-
-
+main()
 
 ########### END YER CODE ABOVE THIS LINE ###########
 
@@ -44,7 +49,7 @@ Tails
 
 1. What was the hardest part of completing this lab? 
 
-
+I have trouble remembering the syntax so i find myself going back to research the exaxt syntax.
 
 
 
@@ -62,6 +67,6 @@ like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Go
 may impede your understanding. Please rate how well you understand the concepts in this lab: 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
+[x] I pretty much get it.
 [ ] I'm solid. Totally got it.
 '''
